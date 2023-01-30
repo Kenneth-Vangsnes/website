9140781231a95097eb6339060867eb8a36f0e0d8
Arfaleas <vangsnesk@gmail.com>
2023-01-30 10:48:03 +0100

    Moved button and functions to their own component

--- src/components/ButtonElement.js
+++ src/components/ButtonElement.js
@@ -1,15 +1,38 @@
-import React, {useState} from "react"
+import React, {useState, useContext} from "react"
+import { Context } from "../Context"
 
 export default function Button(props) {
     const [open, setOpen] = useState(false)
+    const {menu, filteredMenu, setFilteredMenu, setChosenFilters} = useContext(Context)
+    const property = props.props
+    const styles = {backgroundColor: open ? "green" : "gold"}
 
-    function handleClick() {
+    function menuFiltering(property) {
+        const sortedMenu = filteredMenu.length === 0 ? menu : filteredMenu
+        if(property.hasOwnProperty("type") === true) {
+            const filtered = sortedMenu.filter(item => item.type === property.type)
+            setFilteredMenu(filtered)
+            setChosenFilters(prevValues => [...prevValues, property.type])
+        } else if(property.hasOwnProperty("ingredient") === true) {
+            const filtered = sortedMenu.filter(item => item.ingredient.includes(property.ingredient))
+            setFilteredMenu(filtered)
+            setChosenFilters(prevValues => [...prevValues, property.ingredient])
+        } else if(property.hasOwnProperty("allergens") === true) {
+            const filtered = []
+            sortedMenu.map(item => item.allergens.includes(property.allergens) ? null : filtered.push(item)) 
+            setFilteredMenu(filtered)
+            setChosenFilters(prevValues => [...prevValues, property.allergens])
+        } 
         setOpen(oldState => !oldState)
     }
 
     return (
         <button
-            onClick={handleClick}
-        >{open ? "Open" :  "Closed"}</button>
+            style={styles}
+            onClick={() => menuFiltering(property)}
+            disabled={open}
+        >
+            {property.name}
+        </button>
     )
 }
