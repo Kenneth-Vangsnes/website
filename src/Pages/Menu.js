3485dcfeda5e445264f5a9cc2e93ce3d502abc2d
Arfaleas <vangsnesk@gmail.com>
2023-01-30 10:49:39 +0100

    Moved out button and filtering function to own component

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -1,51 +1,23 @@
 import React, {useContext, useState} from "react";
 import MenuElement from "../components/MenuElement";
 import { Context } from "../Context";
+import Button from "../components/ButtonElement"
 
 export default function Menu() {
-    const {menu} = useContext(Context)
-    const [filteredMenu, setFilteredMenu] = useState([])
-    const [chosenFilters, setChosenFilters] = useState([])
-
-    function filter(props) {
-        const sortedMenu = filteredMenu.length === 0 ? menu : filteredMenu
-
-        if(props.hasOwnProperty("type") === true) {
-            const filtered = sortedMenu.filter(item => item.type === props.type)
-            setFilteredMenu(filtered)
-            setChosenFilters(prevValues => [...prevValues, props.type])
-        } else if(props.hasOwnProperty("ingredient") === true) {
-            const filtered = sortedMenu.filter(item => item.ingredient.includes(props.ingredient))
-            setFilteredMenu(filtered)
-            setChosenFilters(prevValues => [...prevValues, props.ingredient])
-        } else if(props.hasOwnProperty("allergens") === true) {
-            const filtered = []
-            sortedMenu.map(item => item.allergens.includes(props.allergens) ? null : filtered.push(item)) 
-            setFilteredMenu(filtered)
-            setChosenFilters(prevValues => [...prevValues, props.allergens])
-        } 
-    }
+    const {menu, filteredMenu, chosenFilters, setFilteredMenu, setChosenFilters} = useContext(Context)
 
     return (
         <main className="menu">
             <h1>Menu</h1>
-            Type:
-            <button onClick={() => filter({type: "appetizer"})}>Appetizer</button>
-            <button onClick={() => filter({type: "soup"})}>Soup</button>
-            <br />
-            Main ingredient:
-            <button onClick={() => filter({ingredient: "chicken"})}>Chicken</button>
-            <button onClick={() => filter({ingredient: "beef"})}>Beef</button>
-            <br />
-            Allergens:  
-            <button onClick={() => filter({allergens: "peanuts"})}>Peanuts</button>
-            <button onClick={() => filter({allergens: "gluten"})}>Gluten</button>
-
-            <br />
+            <Button props={{type: "soup", name: "Soup"}}/>
+            <Button props={{ingredient: "beef", name: "Beef"}}/>
+            
             <button onClick={() => (setFilteredMenu(menu), setChosenFilters([]))}>Reset</button>
             <br />
             {chosenFilters.join(", ")}
-            <MenuElement props={filteredMenu.length === 0 ? menu : filteredMenu} />
+            <MenuElement props={filteredMenu.length === 0 && chosenFilters.length === 0 ? menu : filteredMenu} />
+            <br />
+            {chosenFilters.length !== 0 && filteredMenu.length === 0 ? "No dishes available" : null}
         </main>
     )
 }
